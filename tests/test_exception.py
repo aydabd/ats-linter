93fc5b4fa898987f4d6cbb13751f112ea15a1f17
Aydin Abdi <ayd.abd@gmail.com>
2025-08-30 23:46:51 +0200

    test: add tests for custom exception classes

---
+++ tests/test_exception.py
@@ -0,0 +1,17 @@
+import pytest
+from ats_linter import exception
+
+def test_ATSLinterError_inheritance():
+    err = exception.ATSLinterError("msg")
+    assert isinstance(err, Exception)
+    assert str(err) == "msg"
+
+def test_ATSFileCollectionError_inheritance():
+    err = exception.ATSFileCollectionError("file error")
+    assert isinstance(err, exception.ATSLinterError)
+    assert str(err) == "file error"
+
+def test_ATSASTParseError_inheritance():
+    err = exception.ATSASTParseError("ast error")
+    assert isinstance(err, exception.ATSLinterError)
+    assert str(err) == "ast error"
